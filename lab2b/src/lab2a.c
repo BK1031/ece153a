73019a7a44b11d4e47b0b5a5aae2f557166f8589
Bharat Kathi <bharat1031@gmail.com>
2023-11-09 01:07:13 -0800

    Update lab2a.c

--- lab2b/src/lab2a.c
+++ lab2b/src/lab2a.c
@@ -8,6 +8,8 @@
 #include "qpn_port.h"
 #include "bsp.h"
 #include "lab2a.h"
+#include "lcd.h"
+#include <math.h>
 
 
 
@@ -15,6 +17,15 @@
 typedef struct Lab2ATag  {               //Lab2A State machine
 	QActive super;
 }  Lab2A;
+
+int act_volume = 0;
+int stored_value = 0;
+int alive = 1;
+int Mute = 0;
+int MainVolumeFlag = 0;
+int VolumeFlag = 0;
+int MainTextFlag = 0;
+int TextFlag = 0;
 
 /* Setup state machines */
 /**********************************************************************/
@@ -56,6 +67,47 @@ QState Lab2A_on(Lab2A *me) {
 }
 
 
+void VolumeUp() {
+	printf("Volume: %d\n", act_volume);
+	setColorBg(255, 172, 28);
+	fillRect(act_volume+69, 90, act_volume+70, 110);
+	MainVolumeFlag = 1;
+	VolumeFlag = 1;
+}
+
+void VolumeDown() {
+	printf("Volume: %d\n", act_volume);
+	setColor(0, 0, 0);
+	fillRect(act_volume+70, 90, act_volume+71, 110);
+	MainVolumeFlag = 1;
+	VolumeFlag = 1;
+}
+
+void VolumeToggle(int toggle) {
+	if (toggle == 1) {
+		setColor(0, 0, 0);
+		fillRect(70, 90, 170, 110);
+	} else {
+		setColor(0, 255, 0);
+		fillRect(69, 90, 70+act_volume, 110);
+		MainVolumeFlag = 1;
+		VolumeFlag = 1;
+	}
+}
+
+void OnScreenText(char* s1) {
+	setColor(255, 191, 0);
+	setColorBg(255, 255, 255);
+	setFont(BigFont);
+	lcdPrint(s1, 75, 140);
+	MainTextFlag = 1;
+	TextFlag = 1;
+}
+
+void TimeOutVOLUME() {
+
+}
+
 /* Create Lab2A_on state and do any initialization code if needed */
 /******************************************************************/
 
@@ -65,20 +117,59 @@ QState Lab2A_stateA(Lab2A *me) {
 			xil_printf("Startup State A\n");
 			return Q_HANDLED();
 		}
-		
+
 		case ENCODER_UP: {
 			xil_printf("Encoder Up from State A\n");
+			if (act_volume < 100) {
+				act_volume ++;
+			} else if (act_volume >= 100) {		//Worst case if bugged
+				act_volume = 100;
+			}
+			VolumeUp();
 			return Q_HANDLED();
 		}
 
 		case ENCODER_DOWN: {
 			xil_printf("Encoder Down from State A\n");
+			if (act_volume > 0) {
+				act_volume --;
+			} else if (act_volume <= 0) {		//Worst case if bugged
+				act_volume = 0;
+			}
+			VolumeDown();
 			return Q_HANDLED();
 		}
 
 		case ENCODER_CLICK:  {
 			xil_printf("Changing State\n");
+			Mute = 1;
+			VolumeToggle(Mute);
 			return Q_TRAN(&Lab2A_stateB);
+		}
+		case BTNU: { // UP
+			xil_printf("Up Button from State A\n");
+			OnScreenText("MODE: 1");
+			return Q_HANDLED();
+		}
+		case BTND: { // DOWN
+			xil_printf("Down Button from State A\n");
+			OnScreenText("MODE: 2");
+			return Q_HANDLED();
+		}
+		case BTNC: { // CENTER
+			xil_printf("Center Button from State A\n");
+			OnScreenText("MODE: 3");
+			return Q_HANDLED();
+		}
+		case BTNL: { // LEFT
+			xil_printf("Left Button from State A\n");
+			OnScreenText("MODE: 4");
+			return Q_HANDLED();
+		}
+		case BTNR: { // RIGHT
+			xil_printf("Right Button from State A\n");
+			OnScreenText("MODE: 5");
+			return Q_HANDLED();
 		}
 
 	}
@@ -93,7 +184,7 @@ QState Lab2A_stateB(Lab2A *me) {
 			xil_printf("Startup State B\n");
 			return Q_HANDLED();
 		}
-		
+
 		case ENCODER_UP: {
 			xil_printf("Encoder Up from State B\n");
 			return Q_HANDLED();
@@ -106,7 +197,29 @@ QState Lab2A_stateB(Lab2A *me) {
 
 		case ENCODER_CLICK:  {
 			xil_printf("Changing State\n");
+			Mute = 0;
+			VolumeToggle(Mute);
 			return Q_TRAN(&Lab2A_stateA);
+		}
+		case BTNU: { // UP
+			OnScreenText("MODE:1");
+			return Q_HANDLED();
+		}
+		case BTND: { // DOWN
+			OnScreenText("MODE:2");
+			return Q_HANDLED();
+		}
+		case BTNC: { // CENTER
+			OnScreenText("MODE:3");
+			return Q_HANDLED();
+		}
+		case BTNL: { // LEFT
+			OnScreenText("MODE:4");
+			return Q_HANDLED();
+		}
+		case BTNR: { // RIGHT
+			OnScreenText("MODE:5");
+			return Q_HANDLED();
 		}
 
 	}
